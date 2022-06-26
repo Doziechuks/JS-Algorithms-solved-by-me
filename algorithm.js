8154b839f467f98c653653297e3ff005df6a3e3c
doziechuks <doziechuks1010@gmail.com>
2022-06-26 20:55:11 +0100

    convert each letter of an alphabet in a word or sentence to its corresponding number. Also create a function that outputs the number of likes and their names

--- algorithm.js
+++ algorithm.js
@@ -568,4 +568,51 @@ let my = myFun(2);
 console.log(my);
 
 
+// Replace With Alphabet Position
+
+function alphabetPosition(text) {
+  let aplphaNum = {
+    a: 1, b: 2, c: 3, d: 4, e: 5,
+    f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17,
+    r: 18, s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26
+  };
+
+  let wordSplit = text.toLowerCase().split('');
+  let output = [];
+  for(let i = 0; i < wordSplit.length; i++){
+    
+    if(wordSplit[i] in aplphaNum === false){
+
+      output.push();
+
+    }else{
+
+      output.push(aplphaNum[wordSplit[i]]);
+    }
+  }
+  return output.join(' ');
+}
+
+let output = alphabetPosition('John Doe');
+console.log(output);
+
+
+// Who likes it?
+function likes(names) {
+  let output = '';
+
+  if (names.length === 0) {
+    output += `no ones likes this`;
+  } else if (names.length === 1) {
+    output += `${names} likes this`;
+  } else if (names.length === 2) {
+    output += `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    output += `${names[0]}, ${names[1]} and ${names[1]} like this`;
+  }else{
+    output += `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+
+  return output;
+}
 

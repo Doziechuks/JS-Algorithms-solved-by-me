ff680c3630aaf1d58c968119edb452aaa88c38ad
doziechuks <doziechuks1010@gmail.com>
2022-07-09 10:41:26 +0100

    create a function that moves all the zeros in an array to the end

--- algorithm.js
+++ algorithm.js
@@ -822,3 +822,22 @@ function isNarcissistic(n) {
 
 console.log(isNarcissistic(8208));
 
+
+// Moving zeros to the end
+function moveZeros(arr) {
+  let newArr = [];
+  let zeroArr =[];
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] !== 0){
+      newArr.push(arr[i]);
+    }
+     if(arr[i] === 0){
+       zeroArr.push(arr[i]);
+     }
+  }
+  let arrConcat = newArr.concat(zeroArr);
+  return arrConcat;
+}
+
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
+

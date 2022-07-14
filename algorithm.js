f8839511eaa2a810e5dad04c0d9ea11a40042e1f
doziechuks <doziechuks1010@gmail.com>
2022-07-14 08:58:30 +0100

    wolves in sheep's clothing

--- algorithm.js
+++ algorithm.js
@@ -841,3 +841,31 @@ function moveZeros(arr) {
 
 console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
 
+
+// A wolf in sheep's clothing
+function warnTheSheep(queue) {
+  let sheepidx = queue.length - queue.indexOf('wolf') - 1;
+  if(sheepidx === 0){
+    return "Pls go away and stop eating my sheep";
+  }else{
+    return 'Oi! Sheep number' + sheepidx  + '! You are about to be eaten by a wolf!';
+
+  }
+  // for(let i = 0; i < queue.length; i++){
+  //   if((queue[i].length-1) === 'sheep'){
+  //     return "Oi! Sheep number 1! You are about to be eaten by a wolf!";
+  //   }
+  //   if((queue[i].length-1) === 'wolf'){
+  //     return "Pls go away and stop eating my sheep";
+  //   }
+  // }
+}
+
+console.log(warnTheSheep(["sheep", "sheep", "sheep", "sheep",'sheep', "sheep", 'sheep', 'wolf', 'sheep']));
+
+let Sheep = ["sheep", "sheep", "sheep", 'wolf', 'sheep'];
+let sheepindex = Sheep.length - Sheep.indexOf('wolf') + 1;
+console.log(sheepindex); 
+
+
+

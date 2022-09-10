c53ea29d6ab35f09fe0d85bf4c358cf4780a8860
doziechuks <doziechuks1010@gmail.com>
2022-09-10 20:00:26 +0100

    pick out elements not common in two arrays

--- algorithm.js
+++ algorithm.js
@@ -1292,3 +1292,13 @@ function sumAll(arr) {
 }
 
 console.log(sumAll([1, 4]));
+
+
+// pick out elements not common in two arrays
+function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter((item) => !arr1.includes(item) || !arr2.includes(item));
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
